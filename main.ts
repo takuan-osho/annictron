0a986cbb1e462ed5ef31c23c15b3628cf3361594
SHIMIZU Taku <shimizu.taku@gmail.com>
2015-11-23 17:05:40 +0900

    Initial commit

---
+++ main.ts
@@ -0,0 +1,26 @@
+"use strict";
+
+import app = require("app");
+import BrowserWindow = require("browser-window");
+
+require("crash-reporter").start();
+
+var mainWindow: GitHubElectron.BrowserWindow = null;
+
+app.on("window-all-closed", () => {
+	if (process.platform != "darwin") {
+		app.quit();
+	}
+});
+
+app.on("ready", () => {
+	mainWindow = new BrowserWindow({
+		width: 800,
+		height: 600
+	});
+	mainWindow.loadURL("file://" + __dirname + "/index.html");
+
+	mainWindow.on("closed", () => {
+		mainWindow = null;
+	});
+});
